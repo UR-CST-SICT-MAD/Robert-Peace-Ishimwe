9b26841f772d12c0768c561d5eda7375d57c2ea2
rwema3 <rwemadominik3@gmail.com>
2022-02-21 18:24:55 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -10,8 +10,7 @@ Widget appBarMain(BuildContext context
   
       Icon(Icons.emoji_food_beverage_outlined
   
-
-
+s
   );
 InputDecoration textFieldInputDecoration(String hintText) {
   return InputDecoration(
