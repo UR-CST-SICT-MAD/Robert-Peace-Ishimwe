81333a98a29889ee1ed459db1419302a285b4456
rwema3 <rwemadominik3@gmail.com>
2022-02-21 18:32:33 +0200

    widgets flutter

--- lib/widget.dart
+++ lib/widget.dart
@@ -1,4 +1,23 @@
 import 'package:flutter/material.dart';
 
 Widget appBarMain(BuildContext context
-  ret
+  return 
+    flexibleS
+
+        g
+          colors:
+    leading: Icon(Ico
+  
+      Icon(Icons.emoji_food_beverage_outlined
+  
+
+
+  );
+InputDecoration textFieldInputDecoration(String hintText) {
+  return InputDecoration(
+      hintText: hintText,
+      hintStyle: TextStyle(color: Colors.white
+      focusedBorder:
+          UnrlineInputBorder(borderSide: BorderSide(color: Colors.
+      enabledBor
+          UnderlineInputBorder(borderSide: BorderSide(color: Co
