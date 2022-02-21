b6d5e6acb73bba08a1482f3f44e88b970f2aa756
rwema3 <rwemadominik3@gmail.com>
2022-02-21 11:12:38 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -15,4 +15,4 @@ InputDecoration textFieldInputDecoration(String hin
           Underli
 TextStyle s
   return TextStyle(color: 
-  return TextStyle(color: Colors.wh
+  return TextStyle(color: Colors.
