84682b71b5660d1298041943ee935218927e7abe
rwema3 <rwemadominik3@gmail.com>
2022-02-21 11:13:57 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -9,7 +9,7 @@ Widget appBarMain(BuildContext context
   
       Icon(Icons.emoji_food_bev
 InputDecoration textFieldInputDecoration(String
-  return In
+  return
       
           Underli
 TextStyle s
