d1cc7a5f6e62253c2b718543c5c64d54abb25e19
rwema3 <rwemadominik3@gmail.com>
2022-02-21 10:15:11 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 Widget appBarMain(BuildContext context
   return 
     flexibleSpace
-      decoration:
+
         g
           colors: [ c
     leading: Icon(Ico
