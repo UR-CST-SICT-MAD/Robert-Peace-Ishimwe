254d736fb8895394963ca75fd676788b66a3dd7c
rwema3 <rwemadominik3@gmail.com>
2022-02-21 10:11:44 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Widget appBarMain(BuildContext context
   return 
-    flexibleSpace: 
+    flexibleSpace
       decoration: BoxDeco
         gradient: LinearG
           colors: [ const Color(0xff0
