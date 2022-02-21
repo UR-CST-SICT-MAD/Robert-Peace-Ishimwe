e81b4a6c4f538593d84b7e74390cf9fa58fcc599
rwema3 <rwemadominik3@gmail.com>
2022-02-21 11:58:49 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -15,7 +15,7 @@ InputDecoration textFieldInputDecoration(String hintText
       hintText: hintText,
       hintSt
       
-          UnderlineInputBorder(borderSide: BorderSide(c
+          UnderlineInputBorder(borderSide: BorderSide(col
           UnderlineInputBorder(bord
 TextStyle simpleTe
   return TextStyle(color: Colors.
