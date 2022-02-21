64f403d8d3c1d8a970608e955e08638495357c6f
rwema3 <rwemadominik3@gmail.com>
2022-02-21 11:50:27 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -7,7 +7,7 @@ Widget appBarMain(BuildContext context
           colors:
     leading: Icon
   
-      Icon(Icons.emoji_food_b
+      Icon(Icons.emoji_food_bev
 InputDecoration textFieldInputDecoration(Str
   return
       
