72b002af35454edaeca7bcf9b5e028870cfec963
rwema3 <rwemadominik3@gmail.com>
2022-02-21 11:14:29 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -8,7 +8,7 @@ Widget appBarMain(BuildContext context
     leading: Icon
   
       Icon(Icons.
-InputDecoration textFieldInputDecoration(
+InputDecoration textFieldInputDecorati
   return
       
           Underli
