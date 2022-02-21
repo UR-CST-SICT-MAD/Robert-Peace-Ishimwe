8e2928d9e1a09a404307ead173f4d6d0b923c297
rwema3 <rwemadominik3@gmail.com>
2022-02-21 12:04:30 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -13,7 +13,7 @@ Widget appBarMain(BuildContext context
 InputDecoration textFieldInputDecoration(String hintText
   return InputDecor
       hintText: hintText,
-      hintStyle: TextSty
+      hintStyle: TextStyle(
       focusedBos
           UnderlineInputBorder(borderSide: BorderSide(color: C
           UnderlineInputBorder(borderSi
