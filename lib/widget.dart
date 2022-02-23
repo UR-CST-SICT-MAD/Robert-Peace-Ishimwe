99397254a252a3f5d159895d20b6a307531d06c5
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:37:15 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -11,6 +11,6 @@ Widget appBarMain(BuildContext context) {
       ),
     ),
     leading: Icon(Icons.restaurant),
-    title: Text('Rwema & Robert Resto'),
+    title: Tex('Rwema & Robert Resto'),
     actions: [
       Icon(Ico
