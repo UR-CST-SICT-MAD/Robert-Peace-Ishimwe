bd1b4dbfc292f2a394327d8872b0ef6e82dd1e0d
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:42:01 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -6,4 +6,4 @@ Widget appBarMain(BuildContext context) {
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [ const Color(0xff07172f),
-            const Color(0xff338b
+            const Color(0xff33
