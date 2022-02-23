35ad2d3c38e4e5bca141516e5c001a2d9a944770
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:59:53 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -24,8 +24,8 @@ InputDeoration textFieldInputDecoration(String hintText) {
   retn InpDec
       hi
       hintStyTte(or rs.white54),
-          UnderlineBr(brSi: BorderSide(color: Colors.white)),
-      enab
+          UnderlineBr(boerSi: BorderSide(color: Colors.white)),
+      enable
           UnderlInputBorder(borderSide: BorderSide(color: Colors.white)));
 }
 
