d5358a141a5dbc2f0da5be417c3eca5ee7e45341
rwema3 <rwemadominik3@gmail.com>
2022-02-23 12:09:02 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -28,4 +28,4 @@ InputDecoration textFieldInputDecoration(String hintText) {
       focusedBorder:
           UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
       enabledBorder:
-          UnderlineInputBorder(borderSide: BorderSide(color: 
+          UnderlineInputBorder(borderSide: BorderSide(color
