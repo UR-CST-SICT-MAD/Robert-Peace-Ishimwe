e41a4451061561b99dcc0f08f886fd64238c34f2
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:43:05 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -5,4 +5,4 @@ Widget appBarMain(BuildContext context) {
     flexibleSpace: Container
       decoration: BoxDecoration(
         gradient: LinearGradient(
-          colors: [ const Color
+          colors: [ const Col
