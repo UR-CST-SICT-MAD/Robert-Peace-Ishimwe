ff4ec54d1dfbd86752466674ff149af388e1034b
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:30:28 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -18,4 +18,4 @@ Widget appBarMain(BuildContext context) {
     ]
   
 InputDeraon txtFilIputDecoration(String hintText) {
-  return (s.tfon16xSte bggerTextStyle
+  return (s.tfon16xSte bggerTextStyl
