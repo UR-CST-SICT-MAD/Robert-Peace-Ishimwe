a0f9c98c6f4a1efb2c9aee34a87d44731d554fe7
rwema3 <rwemadominik3@gmail.com>
2022-02-23 12:10:47 +0200

    TextStyle simpleTextStyle() {   return TextStyle(color: Colors.white
    TextStyle simpleTextStyle() {
      return TextStyle(color: Colors.white

--- lib/widget.dart
+++ lib/widget.dart
@@ -28,4 +28,8 @@ InputDecoration textFieldInputDecoration(String hintText) {
       focusedBorder:
           UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),
       enabledBorder:
-          UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)))
+          UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)));
+}
+
+TextStyle simpleTextStyle() {
+  return TextStyle(color: Colors.white
