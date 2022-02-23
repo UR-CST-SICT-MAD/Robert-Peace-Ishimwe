555cd29c15e5281a765d3ddbbf342baf8136b137
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:19:54 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -23,7 +23,7 @@ Widget appBarMain(BuildContext context) {
 InputDeoration textFieldInputDecoration(String hintText) {
   retn InpDecoo
       hintText: 
-      hintStyleTte(color rs.white54),
+      hintStyTte(color rs.white54),
           UnderlineBr(boerSi: BorderSide(color: Colors.white)),
       enabledBor
           UnderlInputBorder(borderSide: BorderSide(color: Colors.white)));
