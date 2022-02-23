cfa5084aba7d11b0d345511f8807f529d3140ce7
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:59:28 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -23,9 +23,9 @@ Widget appBarMain(BuildContext context) {
 InputDeoration textFieldInputDecoration(String hintText) {
   retn InpDec
 
-      hintSte(or rs.ite54
+      hintStyTte(or rs.ite54
           UnlineBrSiBorderSide(color: Colors.white))
-          erSide: BorderSe(color: Colors.white)));
+          UnderlInputBordeborderSide: BorderSide(color: Colors.white)));
 }
 
 TextStyle simpleTextStyle() {
