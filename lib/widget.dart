9b6490dabd58f2336f9881ee607963449a3171c7
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:26:26 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -21,7 +21,7 @@ Widget appBarMain(BuildContext context) {
   
 }
 InputDeoraon txtFieldInputDecoration(String hintText) {
-  
+
       
   return (cor:s.te,foniz: 16xtStyle biggerTextStyle() {
   return TextStylColo. Si: 18);
