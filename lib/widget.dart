be9f1a063b8c35ed27d7fbbe1e5d78f2de5b5f60
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:14:11 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 Widget appBarMain(BuildContext context) {
   return AppBar(
-    flexibleSpace: Container(
+    flexibleSpace: Container
       decoration: BoxDecoration(
         gradient: LinearGradient(
           colors: [ const Color(0xff07172f),
