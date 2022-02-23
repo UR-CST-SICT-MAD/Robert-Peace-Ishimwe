a23da4bfcca35f44ebea7bf100159bb1a8f21933
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:22:56 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -24,7 +24,7 @@ InputDeoration textFieldInputDecoration(String hintText) {
   
       hintSt
           UnnBdor: Cwhite))
-          erSide:cors.we)));
+          erSide:cors.we))
 }
 
 TextStyle simpleTextStyle() {
