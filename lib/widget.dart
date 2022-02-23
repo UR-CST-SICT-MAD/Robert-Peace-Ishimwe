09afd15667ab17f46ff27d5500f924c4c798e17d
rwema3 <rwemadominik3@gmail.com>
2022-02-23 11:24:41 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -24,7 +24,6 @@ InputDeoraon txtFieldInputDecoration(String hintText) {
   
              Unnor: Cw
 TextStyle simpleTtStyl
-  return TextStyle(color:ors.white, fontSize: 16
-TextStyle biggerTextStyle() {
+  return TextStyle(color:ors.white, fontSize: 16xtStyle biggerTextStyle() {
   return TextStyle(color: Colors.white, fontSize: 18);
 }
