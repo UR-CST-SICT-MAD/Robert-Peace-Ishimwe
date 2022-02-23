2a150c5eda255929f3bf265f8a51c9aa518c1ba0
rwema3 <rwemadominik3@gmail.com>
2022-02-23 12:04:43 +0200

    TextStyle simpleTextStyle() {   return TextStyle(color: Colors.white, fontSize: 16); }  TextStyle biggerTextStyle() {   return TextStyle(color: Colors.white, fontSize: 18); }
    TextStyle simpleTextStyle() {
      return TextStyle(color: Colors.white, fontSize: 16);
    }
    
    TextStyle biggerTextStyle() {
      return TextStyle(color: Colors.white, fontSize: 18);
    }

--- lib/widget.dart
+++ lib/widget.dart
@@ -34,7 +34,6 @@ InputDecoration textFieldInputDecoration(String hintText) {
 TextStyle simpleTextStyle() {
   return TextStyle(color: Colors.white, fontSize: 16);
 }
-
 TextStyle biggerTextStyle() {
   return TextStyle(color: Colors.white, fontSize: 18);
 }
