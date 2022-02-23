b47fe1123798bd604c8a4892b4580f207cd01022
rwema3 <rwemadominik3@gmail.com>
2022-02-23 12:07:37 +0200

    Update widget.dart

--- lib/widget.dart
+++ lib/widget.dart
@@ -34,4 +34,4 @@ InputDecoration textFieldInputDecoration(String hintText) {
 TextStyle simpleTextStyle() {
   return TextStyle(color: Colors.white, fontSize: 16);
 }
-TextStyle biggerTextStyle() {
+TextStyle biggerTextStyle()
