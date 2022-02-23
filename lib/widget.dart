847dfc16ee20c88a5ef030d51f677bc9b87dda44
rwema3 <rwemadominik3@gmail.com>
2022-02-23 12:11:48 +0200

    InputDecoration(       hintText: hintText,       hintStyle: TextStyle(color: Colors.white54),       focusedBorder:           UnderlineInputBorder(borderSide: BorderSide(color: Colors.white)),       enabledBorder:           UnderlineInputBorder(borderSide: BorderSide(color: Colors.white))); }

--- lib/widget.dart
+++ lib/widget.dart
@@ -10,7 +10,8 @@ Widget appBarMain(BuildContext context) {
         ),
       ),
     ),
-   & Robert Resto'),
+    leading: Icon(Icons.restaurant),
+    title: Text('Rwema & Robert Resto'),
     actions: [
       Icon(Icons.emoji_food_beverage_outlined),
 
